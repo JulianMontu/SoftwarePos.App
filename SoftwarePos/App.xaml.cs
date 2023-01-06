806cb67992d8cb73c4da3f16a7edcbb669b3822e
JulianMontu <julamontufar@misena.edu.co>
2023-01-05 20:46:33 -0500

    first commit

---
+++ SoftwarePos/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftwarePos;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SoftwarePos/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftwarePos;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
